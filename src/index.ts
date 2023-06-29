b42aecd4e36fc93dbcc131e770b7be7a00feaf5c
mckolles <koless98@icloud.com>
2023-06-29 17:14:14 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 // Обработчик GET запроса на корневой URL
 app.get('/', (req:any, res:any) => {~``
