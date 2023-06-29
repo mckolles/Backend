110fe2fc34631ca6cd2a7fe596c328948c6616d1
mckolles <koless98@icloud.com>
2023-06-29 16:23:42 +0300

    Init commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Обработчик GET запроса на корневой URL
+app.get('/', (req:any, res:any) => {~``
+  res.send('Привет, мир!');
+});
+
+// Обработчик POST запроса на URL '/login'
+app.post('/login', (req:any, res:any) => {
+  res.send('Вы отправили POST запрос на страницу входа');
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
